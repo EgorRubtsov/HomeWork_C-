82c4c3fa3a5d18792b88088d78f7781e853557ff
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-18 02:05:19 +0700

    Определили максимальное из трех чисел

---
+++ Exercise02_MaxOfThreeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter first number: ");
+double number1 = double.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+double number2 = double.Parse(Console.ReadLine());
+Console.Write("Enter third number: ");
+double number3 = double.Parse(Console.ReadLine());
+
+double max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+
+else 
+{
+    max = number1;
+}
+
+if (number3 > max)
+{
+    max = number3;
+}
+
+else
+{
+    max = max;
+}
+
+Console.Write("Max number = ");
+Console.WriteLine(max);
