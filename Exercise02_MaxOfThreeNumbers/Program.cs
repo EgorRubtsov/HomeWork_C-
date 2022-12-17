77ce03b9064f2c59b1a71b67b5784a2eff840eac
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-18 02:14:24 +0700

    Оптимизировали код

--- Exercise02_MaxOfThreeNumbers/Program.cs
+++ Exercise02_MaxOfThreeNumbers/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Enter first number: ");
-double number1 = double.Parse(Console.ReadLine());
+double number1 = double.Parse(Console.ReadLine()!);
 Console.Write("Enter second number: ");
-double number2 = double.Parse(Console.ReadLine());
+double number2 = double.Parse(Console.ReadLine()!);
 Console.Write("Enter third number: ");
-double number3 = double.Parse(Console.ReadLine());
+double number3 = double.Parse(Console.ReadLine()!);
 
 double max = number1;
 
@@ -12,19 +12,9 @@ if (number2 > max)
     max = number2;
 }
 
-else 
-{
-    max = number1;
-}
-
 if (number3 > max)
 {
     max = number3;
-}
-
-else
-{
-    max = max;
 }
 
 Console.Write("Max number = ");
