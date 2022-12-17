f9a79a443ae38183390023be8a1f024db3cc7550
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-17 20:03:27 +0700

    Вывод максимального и минимального чисел

--- Exercise01_MaxOfTwoNumbers/Program.cs
+++ Exercise01_MaxOfTwoNumbers/Program.cs
@@ -4,18 +4,25 @@ Console.Write("Enter second number: ");
 double secondNumber = double.Parse(Console.ReadLine()!);
 
 double max;
+double min;
+
 
 if (firstNumber > secondNumber) 
 {
     max = firstNumber;
+    min = secondNumber;
 }
 
 else
 {
     max = secondNumber;
+    min = firstNumber;
 }
 
 Console.Write("Your Maximum Number: ");
 Console.WriteLine(max);
+Console.WriteLine("and");
+Console.Write("Your Minimum Number: ");
+Console.WriteLine(min);
 
 
