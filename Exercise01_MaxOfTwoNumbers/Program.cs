0eef4a3646f4272a5915c2c84f6d490e89f6c703
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-17 19:54:53 +0700

    Задача№1 на нахождение максимального числа из двух

---
+++ Exercise01_MaxOfTwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter first number: ");
+double firstNumber = double.Parse(Console.ReadLine()!);
+Console.Write("Enter second number: ");
+double secondNumber = double.Parse(Console.ReadLine()!);
+
+double max;
+
+if (firstNumber > secondNumber) 
+{
+    max = firstNumber;
+}
+
+else
+{
+    max = secondNumber;
+}
+
+Console.Write("Your Maximum Number: ");
+Console.WriteLine(max);
+
+
