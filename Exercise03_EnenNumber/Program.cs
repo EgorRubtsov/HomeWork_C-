1b7f2b289767dfc2762c85349eb140e3c9597be6
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-18 14:12:38 +0700

    Проверили число на четность

---
+++ Exercise03_EnenNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите любое число: ");
+double number = double.Parse(Console.ReadLine()!);
+
+if (number%2 == 0)
+
+    Console.WriteLine($"Введенное вами число <{number}> является четным");
+
+
+else 
+
+    Console.WriteLine($"Введенное вами число <{number}> является нечетным");
+
