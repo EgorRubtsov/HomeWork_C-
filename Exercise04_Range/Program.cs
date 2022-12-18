2cc55b2079834b2a6c8447a1b3602724f8f1febf
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-18 15:31:38 +0700

    Четные числа в указанном диапазоне

---
+++ Exercise04_Range/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите любое положительное целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int number1 = 1;
+
+Console.Write($"Четными в ряду чисел от <1> до <{number}> являются числа: ( ");
+
+while (number1 <= number)
+{
+if (number1 % 2 == 0)
+    
+    Console.Write($"{number1}, ");
+    
+    number1++;
+}
+Console.WriteLine(")");
