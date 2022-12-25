b1eed8b97351932e0d595de83e5ae2ffbd5a7303
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-25 22:23:33 +0700

    Определили третью цифру числа, не превышающего 100 000

---
+++ Exercise02_ThirdDigit/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.Write("Введите любое число, не превышающее 100 000: ");
+ int number = int.Parse(Console.ReadLine()!);
+
+if (number > 99 && number < 1000)
+{
+System.Console.WriteLine($"Третья цифра введенного числа: " + (number%10));
+}
+else if (number >= 1000 && number < 10000)
+{
+    System.Console.WriteLine($"Третья цифра введенного числа: " + ((number/10)%10));
+}
+else if (number >= 10000 && number < 100000)
+{
+    System.Console.WriteLine($"Третья цифра введенного числа: " + ((number/100)%10));
+}
+else if (number == 100000)
+{
+    System.Console.WriteLine($"Третья цифра введенного числа: " + ((number/1000)%10));
+}
+
+else 
+{
+    System.Console.WriteLine("Данное число не имеет третьей цифры, либо является отрицательным, либо больше 100 000");
+}
