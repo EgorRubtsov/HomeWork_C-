a9917873094252675fe0e9bbd5c546365f96a8df
Egor <120014929+EgorRubtsov@users.noreply.github.com>
2022-12-26 00:17:38 +0700

    Update Program.cs
    Поправили код

--- Exercise03_DayOfWeek/Program.cs
+++ Exercise03_DayOfWeek/Program.cs
@@ -1,4 +1,4 @@
-﻿int day = new Random(). Next (1, 7);
+﻿int day = new Random(). Next (1, 8);
 if (day >= 1 && day <= 5)
 {
     System.Console.WriteLine($"Сегодня " + (day) + " день недели, рабочий день(:");
