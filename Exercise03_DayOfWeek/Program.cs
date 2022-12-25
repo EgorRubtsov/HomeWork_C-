d37e050e93a68c9c19fe3731f9383720ec208d1a
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-25 23:24:21 +0700

    Рабочий или выходной

---
+++ Exercise03_DayOfWeek/Program.cs
@@ -0,0 +1,9 @@
+﻿int day = new Random(). Next (1, 7);
+if (day >= 1 && day <= 5)
+{
+    System.Console.WriteLine($"Сегодня " + (day) + " день недели, рабочий день(:");
+}
+else
+{
+    System.Console.WriteLine($"Сегодня " + (day) + " день недели, выходной, отдыхаем!");
+}
