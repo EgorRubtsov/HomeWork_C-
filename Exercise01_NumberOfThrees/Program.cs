15199232d503e1f865b9d0e410b385c3a6cce4e0
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-25 21:39:00 +0700

    Определили вторую цифру трехзначного числа

---
+++ Exercise01_NumberOfThrees/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите любое трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 999 || number < 100)
+{
+    System.Console.WriteLine("Ваше число не является трехзначным, введите число от 100 до 999");
+}
+else
+{
+    System.Console.WriteLine($"Вторая цифра вашего числа: " + (number/10)%10);
+}
