385152b043960d124614164327045cd9e68be974
EgorRubtsov <egorrubtsov93@mail.ru>
2022-12-26 23:14:54 +0700

    Определили третью степень числа

---
+++ Exercise01_CubeTable/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Please, enter any positive number: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+double result = 1;
+int index = 1;
+
+while (index <= number)
+{
+    result = Math.Pow(result, 3);
+    System.Console.WriteLine($"Третьей степенью числа " + (index) + " является число " + (result));
+    index++;
+    result = index;
+}
