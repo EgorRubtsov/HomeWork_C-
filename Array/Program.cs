3a95cce0b391f43fa96b78f1bcb46ff92fff2b04
EgorRubtsov <egorrubtsov93@mail.ru>
2023-01-16 00:22:14 +0700

    сгенерировали массив из 8 чисел

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+
+
+
+
+
+
+
+int[] array = new int[8];
+
+void Array(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+
+    Console.Write($"[{String.Join(", ", array)}]");
+
+    System.Console.WriteLine();
+}
+
+Array(array);
