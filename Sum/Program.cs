3ecee6f0b6e0336526143e6388078e8245d26ca2
EgorRubtsov <egorrubtsov93@mail.ru>
2023-01-15 23:35:04 +0700

    посчитали сумму элементов числа

---
+++ Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+
+
+
+
+
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма всех отдельных элементов числа {number} равна: {sumOfElements(number)}");
+
+
+
+ int sumOfElements(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number%10;
+        number = number/10;
+    }
+
+  return sum;
+}
+
+
+
+
