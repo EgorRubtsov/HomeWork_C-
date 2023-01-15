be5352f08b79356b28c6babedec1d6bfd2b89a12
EgorRubtsov <egorrubtsov93@mail.ru>
2023-01-15 21:41:30 +0700

    написали цикл для возведения в степень

---
+++ Cicle/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (Задачи, решенные через Math.Pow, 
+// будут отправлены на переделку, так как задача стоит в том, чтобы написать цикл
+
+
+
+int degreeOfnumber(int firstNumber, int secondNumber)
+{
+   
+
+    int result = firstNumber;
+
+    for(int i = 2; i <= secondNumber; i++)
+    {
+         result = result * firstNumber;
+    }
+    System.Console.WriteLine($"Результатом возведения числа: {firstNumber} в степень: {secondNumber} является: {result}");
+    
+    return result;
+    
+}
+
+
+degreeOfnumber(new Random().Next(2, 7), new Random().Next(2, 7));
+
