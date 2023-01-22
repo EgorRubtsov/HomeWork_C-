ec2e41b32baefe577ed052d2709d2d875f71d599
EgorRubtsov <egorrubtsov93@mail.ru>
2023-01-23 04:28:15 +0700

    Определили четные числа в массиве

---
+++ Array1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+
+int[] Massiv (int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random(). Next(99, 1000);
+    }
+
+    System.Console.WriteLine($"[{String.Join(", ", array)}]");
+
+    return array;
+}
+
+
+int[] array = Massiv(17);
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0) count++;
+}
+
+System.Console.WriteLine("количество чётных чисел в массиве: " + (count));
+
+
