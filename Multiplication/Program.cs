c341f9d981e32644b7f6e6b38b8bdb75ec6a84b9
EgorRubtsov <egorrubtsov93@mail.ru>
2023-01-24 22:53:37 +0700

    доработали код

--- Multiplication/Program.cs
+++ Multiplication/Program.cs
@@ -8,7 +8,7 @@ int[] Massiv (int size)
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random(). Next(13);
+        array[i] = new Random(). Next(9);
     }
 
     System.Console.WriteLine($"[{String.Join(", ", array)}]");
@@ -18,32 +18,35 @@ int[] Massiv (int size)
 }
 
 
-int[] array = Massiv(13);
-System.Console.Write("[");
-
-int  multiplication;
+int[] array = Massiv(11);
+{
+int lentharray = array.Length / 2;
+if (array.Length % 2 > 0) lentharray++;
+int[]  multiplication = new int[lentharray];
 int j = array.Length;
 
 
 for(int i = 0; i < array.Length/2; i++)
 
     { 
-        multiplication = array[i] * array[j - 1];
+        multiplication[i] = array[i] * array[j - 1];
         j--;
-        System.Console.Write($"{String.Join(", ", multiplication + ", ")}");
     }
-
 if(array.Length % 2 > 0)
-{
-int A = array[array.Length / 2 ];
-System.Console.Write(A);
-System.Console.WriteLine("]");
+
+    multiplication[array.Length/2] = array[array.Length / 2];
+
+   
+System.Console.Write($"[{String.Join(", ", multiplication)}]");
+
+System.Console.WriteLine();
 }
-else
-{
-    System.Console.Write("]");
-    System.Console.WriteLine();
-}
+
+
+
+
+
+
 
 
 
