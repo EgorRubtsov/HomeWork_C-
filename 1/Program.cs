98bab26d6c28e7d6e68efe02035d10017b7cdfc5
EgorRubtsov <egorrubtsov93@mail.ru>
2023-01-26 04:49:36 +0700

    1

---
+++ 1/Program.cs
@@ -0,0 +1,30 @@
+﻿// / Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+// Для строки можно использовать данный метод string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+// (пользователь вводит в консоли числа через пробел и они формируют строку)
+
+
+
+
+System.Console.WriteLine("Введите числа: ");
+
+
+
+ int[] numS = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+
+
+int count = 0;
+
+for(int i = 0; i < numS.Length; i++)
+{
+    if(numS[i] > 0) count++;
+
+}
+  
+
+System.Console.WriteLine($"Вы ввели {count} положительных чисел");
