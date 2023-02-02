26ec9061f38a6288ae608f41968dc7e84ce1b3c6
EgorRubtsov <egorrubtsov93@mail.ru>
2023-02-02 11:38:31 +0700

    отсортировали массив по убыванию

---
+++ MassiveSort/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+Console.Write("Введите количество строк: "); 
+int rows = Convert.ToInt32(Console.ReadLine()!); 
+ 
+Console.Write("Введите количество столбцов: "); 
+int columns = Convert.ToInt32(Console.ReadLine()); 
+
+
+
+int[,] array = GetArray(rows, columns, 0, 44); 
+PrintArray(array); 
+System.Console.WriteLine();
+Method(array);
+PrintArray(array);
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue){
+    int[,] result = new int[m,n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            result[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void Method(int [,] array)
+{
+     for(int i = 0; i < array.GetLength(0); i++)
+     {
+        
+        
+            for(int j = 0; j < array.GetLength(1) ; j++)
+            {
+                for(int k = 0; k < array.GetLength(1) - 1 ; k++)   
+
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+
+                }
+            }
+        
+     }
+}
