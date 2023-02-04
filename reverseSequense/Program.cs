05d1a81b1d09f50f3a5404c534bb903834fad9ee
EgorRubtsov <egorrubtsov93@mail.ru>
2023-02-04 22:00:02 +0700

    распечатали числовой ряд

---
+++ reverseSequense/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+
+
+
+
+
+System.Console.WriteLine("Введите первое число вашего числового ряда: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string reverseSequence = new string(Recursion(N, 1));
+
+
+System.Console.WriteLine(reverseSequence);
+
+
+string Recursion(int firstNumber, int lastNumber)
+{
+    if (firstNumber == lastNumber) return firstNumber.ToString();
+    return (firstNumber + ", " + Recursion(firstNumber - 1, lastNumber));
+}
+
+
